9101aa17479890c96428950665786cf33faa8f10
Viktor <toffen2011@gmail.com>
2020-03-02 10:45:25 +0100

    header (nav and design) changes. working on a more structured look

---
+++ JavaScript/scripts.js
@@ -0,0 +1,20 @@
+//const figures = document.getElementsByName("test");
+/*test.onclick = function(mouseEvent) {
+    figAnim(test);
+}
+
+for (let i=0; i<figures.length; i++) {
+    figures[i].onclick = function(mouseEvent) {
+        figAnim(figures[i]);
+    }
+}*/
+
+/*
+function figAnim(test) {
+    /!*test.onclick = function (e) {
+        test.classList.add("clicked");
+    };*!/
+    test.onclick = function (e) {
+        test.style.transform = "scale(0.9)";
+    };
+}*/
