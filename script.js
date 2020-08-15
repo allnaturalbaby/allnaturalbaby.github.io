ca60701ce376a976ae3a09e092386aa54aa56701
Viktor <toffen2011@gmail.com>
2020-08-15 15:06:30 +0200

    kinda got the clock working

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const myTime = document.querySelector("#myTime");
 
-let targetTime = new Date();
-let timeZoneSite = 2;
-let tzDiff = timeZoneSite * 60 + targetTime.getTimezoneOffset();
-let offsetTime = new Date(targetTime.getTime() + tzDiff * 60 * 1000);
+setInterval(function() {
+    let targetTime = new Date();
+    let timeZoneSite = 2;
+    let tzDiff = timeZoneSite * 60 + targetTime.getTimezoneOffset();
+    let offsetTime = new Date(targetTime.getTime() + tzDiff * 60 * 1000);
+    myTime.innerHTML = offsetTime;
+}, 500);
