7496d74ecd633320a4ed1b9002a683726bc5eba5
Viktor <toffen2011@gmail.com>
2020-08-15 15:13:40 +0200

    voilaaaaa it works

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const myTime = document.querySelector("#myTime");
+const myTime = document.querySelector("#timeDisplay");
 
 setInterval(function() {
     let targetTime = new Date();
     let timeZoneSite = 2;
     let tzDiff = timeZoneSite * 60 + targetTime.getTimezoneOffset();
     let offsetTime = new Date(targetTime.getTime() + tzDiff * 60 * 1000);
-    myTime.innerHTML = offsetTime;
+    myTime.innerHTML = `${offsetTime.getHours()}:${offsetTime.getMinutes()}:${offsetTime.getSeconds()}`;
 }, 500);
