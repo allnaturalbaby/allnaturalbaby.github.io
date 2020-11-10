2f755a68ee664bcf3216526df492fc2bff278316
allnaturalbaby <49074160+allnaturalbaby@users.noreply.github.com>
2020-11-10 09:17:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const myTime = document.querySelector("#timeDisplay");
 
 setInterval(function() {
     let targetTime = new Date();
+    // timezone is not dynamic. 1 = normal, 2 = daylight saving
     let timeZoneSite = 1;
     let tzDiff = timeZoneSite * 60 + targetTime.getTimezoneOffset();
     let offsetTime = new Date(targetTime.getTime() + tzDiff * 60 * 1000);
